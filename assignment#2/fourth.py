96f699364d0400b570e52fec7cc2340825fe0039
AsuraA93 <56329129+JamesAsuraA93@users.noreply.github.com>
2021-09-05 17:17:35 +0700

    Explaining

--- assignment#2/fourth.py
+++ assignment#2/fourth.py
@@ -1,3 +1,22 @@
 def is_overlapped(x1,y1,w1,h1,x2,y2,w2,h2):
     return False if (((x1) - (x2+w2) > 0) or ((y2)-(y2+h2) > 0) or ((x2)-(x1+w1)) > 0 or ((y2)-(y1+h1)) > 0) else True
 
+"""
+return False if (conditions) else True
+
+แปลได้ว่า
+
+if <conditions>:
+    return False
+else:
+    return True
+"""
+
+# ซึ่งข้อนี้จะได้เป็น
+
+"""
+if ((x1) - (x2+w2) > 0) or ((y2)-(y2+h2) > 0) or ((x2)-(x1+w1)) > 0 or ((y2)-(y1+h1)) > 0:
+    return False
+else:
+    return True
+"""
